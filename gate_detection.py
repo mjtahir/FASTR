97b396871b374df685495d8fcccf7c43e67fa07a
Muddasir <mtah7421@uni.sydney.edu.au>
2019-04-21 20:45:28 +1000

    Finalised speed testing. Using sort with resized video and tighter epsilon

--- gate_detection.py
+++ gate_detection.py
@@ -13,12 +13,13 @@ parser = argparse.ArgumentParser(description='Run video, image or webcam.')
 parser.add_argument('-v', '--video', help="Path to the (optional) video file.")
 args = parser.parse_args()
 
+# Use webcam if no video file input else use the video
 if not args.video:
-	vid = cv.VideoCapture(0)
+	vid = cv.VideoCapture(0)	# webcam
 else:
-	vid = cv.VideoCapture(args.video)
+	vid = cv.VideoCapture(args.video)	# video
 
-start = timer()
+#start = timer()
 # Loop through each frame
 while True:
 
@@ -51,64 +52,68 @@ while True:
 	# Find the contours in the HSV mask. RETR_LIST does not compute heirachy
 	# APPROX_SIMPLE saves memory by reducing number of points on straight line
 	contours, _ = cv.findContours(mask_hsv, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+	
+	# Sort contours from largest to smallest based on area
 	contours = sorted(contours, key = cv.contourArea, reverse = True)
 
 	# If at least 1 contour found
 	if len(contours) != 0:
-
-		# Define max area contour and loop through each contour
-		max_cnt = 20
-		#i = 0
-		for cnt in contours:
-
-			# Find the perimeter of cnt, closed loop contour. And its area
-			perimeter_cnt = cv.arcLength(cnt, True)
-			area_cnt = cv.contourArea(cnt)
-			#print(area_cnt)
-			#i += 1
-			# Find max deviation from contour allowed to occur for approx.
-			epsilon = 0.015 * perimeter_cnt
-
-			# Finds a contour for cnt, epsilon fitting factor and closed
-			approx_cnt = cv.approxPolyDP(cnt, epsilon, True)
-
-			# If quadrilateral shape and its area is > than current max area
-			if len(approx_cnt) == 4 and area_cnt > max_cnt:
-
-				# Find x, y (top left box coords) and w, h (width and height) 
-				# of the bounding box. Calculate aspect ratio.
-				x, y, w, h = cv.boundingRect(cnt)
-				aspect_ratio = float(w) / h
-
-				# If aspect ratio is ~= 1 then its a square (gate) therefore
-				# store it as the current largest contour
-				#if aspect_ratio >= 0.90 and aspect_ratio <= 1.1:
-				max_cnt = area_cnt
-				drawn_cnt = cnt
-				break
-				#print('break \n')
-			
-		#print(i, '-------- \n')
-		if max_cnt != 20:
-			# Draw contours on img, biggest one, all of them, color, thickness
-			cv.drawContours(img, drawn_cnt, -1, (0, 255, 0), 2)
 		
-			# Plot rectangle, coords, color, thickness
-			cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 255), 1)
+		# Define min area for contour and loop through each contour
+		cnt_area = 20
+		for cnt in contours:
+			
+			# Find current contour's area
+			area_cnt = cv.contourArea(cnt)
 
-			# Compute the centre of the bounding box and plot (on img, using centre
-			# coords, radius, color, filled)
-			centre = (int(x + w/2), int(y + h/2))
-			cv.circle(img, centre, 5, (255, 255, 255), -1)
+			# If area is bigger than the min (cnt_area)
+			if area_cnt > cnt_area:
+
+				# Find the perimeter of cnt, closed loop contour.
+				perimeter_cnt = cv.arcLength(cnt, True)
+				
+				# Find max deviation from contour allowed to occur for approx.
+				# Smaller number defines stricter error requirement
+				epsilon = 0.015 * perimeter_cnt
+
+				# Finds an approx contour for cnt, epsilon fitting factor and 
+				# closed
+				approx_cnt = cv.approxPolyDP(cnt, epsilon, True)
+
+				# If quadrilateral shape
+				if len(approx_cnt) == 4:
+
+					# Find x, y (top left box coords) and w, h (width and height) 
+					# of the bounding box. Calculate aspect ratio.
+					x, y, w, h = cv.boundingRect(cnt)
+					aspect_ratio = float(w) / h
+
+					# If aspect ratio is ~= 1 then its a square (gate) therefore
+					# store it as the current largest contour
+					#if aspect_ratio >= 0.90 and aspect_ratio <= 1.1:
+					cnt_area = area_cnt
+			
+					# Draw contours on img, biggest one, all of them, color, thickness
+					cv.drawContours(img, cnt, -1, (0, 255, 0), 2)
+				
+					# Plot rectangle, coords, color, thickness
+					cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 255), 1)
+
+					# Compute the centre of the bounding box and plot (on img, 
+					# using centre coords, radius, color, filled)
+					centre = (int(x + w/2), int(y + h/2))
+					cv.circle(img, centre, 5, (255, 255, 255), -1)
+
+					# Largest cnt meeting requirements found therefore break
+					break
 
 		# Compute distance from centre of image to centre of box
 
-	#cv.namedWindow("Frame", cv.WINDOW_NORMAL)
-	#cv.resizeWindow("Frame", 600, 400)
+	cv.namedWindow("Frame", cv.WINDOW_NORMAL)
+	cv.resizeWindow("Frame", 600, 400)
+	cv.imshow("Frame", img)
 	#cv.imshow("Source", src)
 	#cv.imshow("Blurred", blur)
-	cv.imshow("Frame", img)
-
 	#cv.imshow("Mask HSV", mask_hsv)
 
 	#cv.waitKey(0)
@@ -116,7 +121,7 @@ while True:
 	if cv.waitKey(1) & 0xFF == ord('q'):
 		break
 
-end = timer()
-print(end - start)
+#end = timer()
+#print(end - start)
 vid.release()
 cv.destroyAllWindows()
