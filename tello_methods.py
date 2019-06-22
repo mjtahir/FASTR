992e119634725f948851e636e659638030fc98d9
Muddasir <mtah7421@uni.sydney.edu.au>
2019-06-22 21:48:10 +1000

    Added class variables for constants, added more doc strings, adjusted shutdown method. No Major changes

--- tello_methods.py
+++ tello_methods.py
@@ -6,6 +6,30 @@ import cv2 as cv
 # import numpy as np
 
 class Tello:
+	'''
+	The Tello class interfaces with the Tello drone based on the Tello SDK v1.3:
+	https://dl-cdn.ryzerobotics.com/downloads/tello/20180910/Tello%20SDK%20Documentation%20EN_1.3.pdf
+	'''
+
+	# Tello address based on IP and port
+	TELLO_IP = '192.168.10.1'
+	TELLO_PORT = 8889
+	TELLO_ADDRESS = (TELLO_IP, TELLO_PORT)
+
+	# Local address
+	LOCAL_IP = '0.0.0.0'
+	LOCAL_PORT = 8889
+	LOCAL_ADDRESS = (LOCAL_IP, LOCAL_PORT)
+
+	# Video port for receiving
+	VIDEO_PORT = 11111
+
+	# Wait time between commands (s)
+	command_timeout = 0.3
+
+	# Address used by openCV function 'VideoCapture()' (pointer to camera)
+	# Found at: https://github.com/damiafuentes/DJITelloPy/blob/master/djitellopy/tello.py
+	camera_address = ('udp://@' + LOCAL_IP + ':' + str(VIDEO_PORT)) #'?overrun_nonfatal=1&fifo_size=5000'
 
 	def __init__(self):
 
@@ -13,22 +37,14 @@ class Tello:
 		self.response = None
 		self.cap = None		# Video object
 		self.frame = None   # Video frame
-		self.command_timeout = 0.3	# Time for response (secs)
-
-		# Address based on IP and port
-		self.TELLO_IP = '192.168.10.1'
-		self.TELLO_PORT = 8889
-		self.TELLO_ADDRESS = (self.TELLO_IP, self.TELLO_PORT)
+		self.streamon = False
 
 		# Socket for commands and video. IPv4, with UDP
 		self.socket_cmd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.socket_video = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 		# Bind the local address to enable communication
-		self.LOCAL_IP = '0.0.0.0'
-		self.LOCAL_PORT = 8889
-		self.LOCAL_ADDRESS = (self.LOCAL_IP, self.LOCAL_PORT)
-		self.socket_cmd.bind(self.LOCAL_ADDRESS)
+		self.socket_cmd.bind(Tello.LOCAL_ADDRESS)
 
 		# Recevier thread - Daemon True so it closes when main thread ends
 		self.receive_thread = threading.Thread(target=self._udpReceive, 
@@ -36,101 +52,76 @@ class Tello:
 		self.receive_thread.start()
 
 		# Send a byte string "command" to initiate Tello's SDK mode
-		self.socket_cmd.sendto(b'command', self.TELLO_ADDRESS)
-		print('Sent: command')
-
-		# Video port for receiving
-		self.TELLO_VIDEO_PORT = 11111
-		self.VIDEO_ADDRESS = (self.LOCAL_IP, self.TELLO_VIDEO_PORT)
-		# self.socket_video.bind(self.VIDEO_ADDRESS)
+		self.sendCommand('command')
+		#self.socket_cmd.sendto(b'command', TELLO_ADDRESS)
+		#print('Sent: command')
 
 	def _udpReceive(self):
 		''' Method runs as a thread to constantly receive responses '''
 		while True:
 			try:
 				self.response, addr = self.socket_cmd.recvfrom(2048)
-				print(self.response.decode('utf-8'))
+				print('Response: ' + self.response.decode('utf-8'))
 			except Exception as exc:
-				print('Exception in udpReceiver. socket.error:', exc)
+				print('Exception in udpReceiver:', exc)
 	
 	def startVideoCapture(self):
-		''' Initiates video capture by starting the Tello camera, finding the 
+		'''
+		Initiates video capture by starting the Tello camera, finding the 
 		pointer to the video (VideoCapture) and starting the thread which reads
 		each frame
 		 '''
-		# Send a byte string "streamon" to initiate Tello's camera stream
-		self.socket_cmd.sendto(b'streamon', self.TELLO_ADDRESS)
-		print('Sent: streamon')
+		self.sendCommandNoWait('streamon')
+		self.streamon = True
 
-		# Retreive pointer to camera. The address was found online
-		# Then start threading
-		self.cap = cv.VideoCapture('udp://@' + self.LOCAL_IP + ':' 
-					+ str(self.TELLO_VIDEO_PORT)) #'?overrun_nonfatal=1&fifo_size=5000'
+		# Start video capture and thread
+		self.cap = cv.VideoCapture(Tello.camera_address)
+		self.ret, self.frame = self.cap.read()	# Manual first frame read
 		self.video_thread = threading.Thread(target=self._updateFrame, 
 							daemon=True)
 		self.video_thread.start()
-		time.sleep(1)		# Initial frames takes time
 
 	def _updateFrame(self):
 		''' Updates frame through a thread '''
-		while True:
+		while self.streamon:
 			try:
 				self.ret, self.frame = self.cap.read()
 			except Exception as exc:
-				print('Exception in updateFrame. cap.read:', exc)
+				print('Exception in _updateFrame:', exc)
 
 	def readFrame(self):
-		''' Returns frame to the calling object '''
+		''' Returns latest frame to the calling object '''
 		return self.frame
 
 	def shutdown(self):
 		''' Shutdown procedure, stop video capture and close sockets '''
-		self.stopVideoCapture()
+		if self.streamon:
+			self.stopVideoCapture()
+
 		self.socket_cmd.close()
 		self.socket_video.close()
 
 	def stopVideoCapture(self):
-		self.socket_cmd.sendto(b'streamoff', self.TELLO_ADDRESS)
-		print('Sent: streamoff')
-		self.cap.release()
+
+		# if self.cap is not None:
+		# 	print('Releasing stream')
+		# 	self.cap.release()
+		self.streamon = False
+		self.sendCommandNoWait('streamoff')
 
 	def sendCommand(self, command):
-		''' Sends utf8 encoded command to the Tello and sleeps before sending
+		'''
+		Sends utf8 encoded command to the Tello and sleeps before sending
 		next command to allow time for a response.
 		'''
 		print('--> Command sent:', command)
-		self.socket_cmd.sendto(command.encode('utf8'), self.TELLO_ADDRESS)
-		time.sleep(self.command_timeout)
-		# Set abort_flag to False and create a timer thread which runs after
-		# command_timeout seconds and sets abort_flag to True
-		# self.abort_flag = False
-		# timer = threading.Timer(self.command_timeout, self._setAbortFlag)
-		# self.socket_cmd.sendto(command.encode('utf-8'), self.TELLO_ADDRESS)
+		self.socket_cmd.sendto(command.encode('utf8'), Tello.TELLO_ADDRESS)
+		time.sleep(Tello.command_timeout)
 
-		# # Start the timer and continually check for response from Tello
-		# timer.start()
-		# while self.response is None:
-		# 	if self.abort_flag is True:
-		# 		break
-		# timer.cancel()
-		
-		# if self.response is None:
-		# 	response = 'none_response'
-		# else:
-		# 	response = self.response.decode('utf-8')
-
-		# # Reset self.response to None and return the recorded response
-		# self.response = None
-		# return response
-
-	# def _setAbortFlag(self):
-	# 	''' Sets the abort_flag to True utilised in sendCommand() '''
-	# 	self.abort_flag = True
-
-	def sendCommandNoResponse(self, command):
+	def sendCommandNoWait(self, command):
 		''' Sends utf8 encoded command to the Tello '''
 		print('--> Command sent:', command)
-		self.socket_cmd.sendto(command.encode('utf8'), self.TELLO_ADDRESS)
+		self.socket_cmd.sendto(command.encode('utf8'), Tello.TELLO_ADDRESS)
 
 	# Control Commands
 	def takeoff(self):
@@ -146,8 +137,9 @@ class Tello:
 		return self.sendCommand('emergency')
 
 	def move(self, direction, distance):
-		''' Move directions include up, down, left, right, forward, back
-		 and distance ranges from 20 - 500 cm
+		'''
+		Move directions include up, down, left, right, forward, back 
+		and distance ranges from 20 - 500 cm
 		'''	
 		return self.sendCommand(direction + ' ' + str(distance))
 
@@ -161,10 +153,11 @@ class Tello:
 		return self.sendCommand('speed ' + str(speed))
 
 	def rc(self, lr=0, fb=0, ud=0, yaw=0):
-		''' Allows for 4 channel remote controller type commands to be sent.
-			The limits for each input is -100 to +100 (percent?)
 		'''
-		return self.sendCommandNoResponse('rc ' + str(lr) + ' ' + str(fb)
+		Allows for 4 channel remote controller type commands to be sent. 
+		The limits for each input is -100 to +100 (percent?)
+		'''
+		return self.sendCommandNoWait('rc ' + str(lr) + ' ' + str(fb)
 			+ ' ' + str(ud) + ' ' + str(yaw))
 
 	# Read Commands
@@ -181,9 +174,10 @@ class Tello:
 		return self.sendCommand('time?')
 
 	def getHeight(self):
-		''' Returns height in dm (decimeter) from starting point 
-			This may be IMU based hence experiences drift. The return
-			value is rounded to the nearest decimeter.
+		''' 
+		Returns height in dm (decimeter) from starting point 
+		This may be IMU based hence experiences drift. The return 
+		value is rounded to the nearest decimeter.
 		'''
 		return self.sendCommand('height?')
 
@@ -192,25 +186,29 @@ class Tello:
 		return self.sendCommand('temp?')
 
 	def getAttitude(self):
-		''' Returns IMU attitude of pitch, roll and yaw in degrees. 
-			Yaw is zeroed when Tello is turned on.
+		''' 
+		Returns IMU attitude of pitch, roll and yaw in degrees. 
+		Yaw is zeroed when Tello is turned on.
 		'''
 		return self.sendCommand('attitude?')
 
 	def getBaro(self):
-		''' Returns barometer value (altitude) in metres.
-			Seems to fluctuate a fair amount
+		'''
+		Returns barometer value (altitude) in metres.
+		Seems to fluctuate a fair amount
 		 '''
 		return self.sendCommand('baro?')
 
 	def getAcceleration(self):
-		''' Returns IMU acceleration in x, y and z directions in
-			0.001g's. So 1000 = 1g.
+		'''
+		Returns IMU acceleration in x, y and z directions in 
+		0.001g's. So 1000 = 1g.
 		 '''
 		return self.sendCommand('acceleration?')
 
 	def getToF(self):
-		''' Returns distance from the bottom mounted 'Time-of-Flight Camera'
+		'''
+		Returns distance from the bottom mounted 'Time-of-Flight Camera'
 		in mm. Minimum is 100 mm and anything below 300 mm defaults to 100
 		mm. Seems fast and accurate.
 		'''
@@ -223,6 +221,7 @@ class Tello:
 
 if __name__ == "__main__":
 	drone = Tello()
+	drone.getBattery()
 	drone.startVideoCapture()
 	
 	while True:
@@ -231,4 +230,3 @@ if __name__ == "__main__":
 		if cv.waitKey(1) & 0xFF == ord('q'):
 			drone.shutdown()
 			break
-	drone.getBattery()
