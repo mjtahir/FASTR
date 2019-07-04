019409a1146c59d0e4b782b705adc344038deeb1
Muddasir <mtah7421@uni.sydney.edu.au>
2019-07-04 13:51:18 +1000

    Tested the assert in simpleDistCalibration and cleaned up some comments.

--- pose_estimation.py
+++ pose_estimation.py
@@ -1,17 +1,18 @@
 def simpleDistCalibration(img, GATE_WIDTH, DISTANCE_TO_GATE):
 	'''
-	The calibration image is assumed to contain a detectable gate which is upright
-	and not angled.
+	The calibration image is assumed to contain a detectable gate which is 
+	upright and not angled.
 	'''
 	from gate_detection import colourSegmentation
 
 	# Width of the gate from colourSegmentation function
-	#_, _, _, PIXEL_WIDTH, _ = colourSegmentation(img)
 	_, cs_coords = colourSegmentation(img)
 	_, _, PIXEL_WIDTH, _, _ = cs_coords
 	
-	assert (PIXEL_WIDTH is not None), "colourSegmentation() returned pixel width \
-		of {}".format(PIXEL_WIDTH)
+	# Throw error if gate is not detected
+	assert (PIXEL_WIDTH is not None), ("colourSegmentation() returned pixel "
+		"width of None. Use a different calibration image where the gate can "
+		"be detected.")
 
 	# Constant focal length per pixel 
 	FOCAL_LENGTH_PER_PIXEL = PIXEL_WIDTH * DISTANCE_TO_GATE / GATE_WIDTH
@@ -19,14 +20,7 @@ def simpleDistCalibration(img, GATE_WIDTH, DISTANCE_TO_GATE):
 
 
 def simpleDist(FOCAL_LENGTH_PER_PIXEL, GATE_WIDTH, pixel_width):
-	# Calibration image: name.png
-	# Distance to gate = 100 cm
-	# Width of gate = 52 cm
-
-	# measure 1: 324 cm
-	# measure 2: 201 cm
-
-
+	''' Calculates the distance given the calibration and current pixel width'''
 	# Calculate distance to the gate
 	if pixel_width is not None:
 		return GATE_WIDTH * FOCAL_LENGTH_PER_PIXEL / pixel_width
