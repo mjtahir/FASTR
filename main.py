76ad79d4c9f145ce1682a19528b7d998bb97b7b8
Muddasir <mtah7421@uni.sydney.edu.au>
2019-11-13 15:32:48 +1100

    Mostly added data logging stuff, changed optitrack true state because of the 2nd rigid body.

--- main.py
+++ main.py
@@ -11,61 +11,15 @@ from controller import runPID, runEdgeCont, runWaypoint
 from localisation import (startTracking, trueState, waypointGeneration, 
 		waypointUpdate, estimatedState)
 
-# USE Z, Y DISTANCE FROM PnP AS ERROR FOR LEFT/RIGHT AND UP/DOWN
 
-def initialiseDataLogger():
-	f = open('Outputs/data_log.txt', 'w+')
-	header_labels = ["Timestamp", "x_true (cm)", "y_true (cm)", "z_true (cm)", 
-					 "q0_true", "q1_true", "q2_true", "q3_true", 
-					 "x_est (cm)", "y_est (cm)", "z_est (cm)", "phi_est (rad)",
-					 "theta_est (rad)", "psi_est (rad)", "Mode", "Distance (cm)"]
-	header_string = "#" + ', '.join(header_labels)
-	f.write(header_string)
-	f.write('\n')	# new line after header
+def logData(data_dict):
+	data_dict_string = str(list(data_dict.values()))
+	data_dict_string = data_dict_string.replace("'", '')
+	data_dict_string = data_dict_string[1:-1]
+	data_dict_string = data_dict_string.replace('None', 'NaN')
 
-	# The H264 codec is automatically changed to AVC1, results in small file size
-	fourcc = cv.VideoWriter_fourcc('H','2','6','4')
-	video_fps = 30	# Tello's FPS is 30
-	video_out = cv.VideoWriter('Outputs/video.mp4', fourcc, video_fps, config.RESOLUTION)
-
-	return f, video_out
-
-
-def logData(file_handle, time, true_state, estimated_state, mode, distance):
-	# If no optitrack data then set arrays to None
-	if true_state is None:
-		true_position = ["NaN","NaN","NaN"]
-		true_quat = ["NaN","NaN","NaN","NaN"]
-	else:
-		true_position = true_state[1]
-		true_quat = true_state[2]
-
-	# if no state was estimated
-	if estimated_state is None:
-		estimated_position = ["NaN","NaN","NaN"]
-		estimated_euler = ["NaN","NaN","NaN"]
-	else:
-		estimated_position = estimated_state[0]
-		estimated_euler = estimated_state[1]
-
-	if mode is None:
-		mode = "NaN"
-
-	if distance is None:
-		distance = "NaN"
-
-	# Form a list of the data in the appropriate order as the header_labels
-	data = [time] + list(true_position) + list(true_quat) + list(estimated_position) + \
-		list(estimated_euler) + [mode, distance]
-
-	# Convert to string
-	data_str = str(data)
-	data_str = data_str.replace("'", '')	# remove the quote symbols from string
-	data_str = data_str[1:-1]	# remove the list brackets at both ends
-
-	# Write to file and add new line for next iteration of data log
-	file_handle.write(data_str)
-	file_handle.write('\n')
+	# Log the data to file on a new line
+	logging.info('%s', data_dict_string)
 
 # Initialise Tello
 tello = Tello()
@@ -74,20 +28,18 @@ tello.startVideoCapture(method="H264Decoder")
 tello.startStateCapture()
 tello.rc()	# reset controls to zeros
 
-fly = False
+fly = True
 if fly:
 	tello.takeoff()
 	time.sleep(8)
-	# tello.move('up',100)
-	# time.sleep(5)
+	tello.move('up',100)
+	time.sleep(5)
 
 # Setup connection with optitrack and the waypoints
 optitrack = False
 if optitrack:
-	streamingClient = startTracking(14)	# Get rigid body ID from Motive
+	streamingClient = startTracking(4, 3)	# Get rigid body ID from Motive
 	waypoint = waypointGeneration(streamingClient)
-
-log_file, video_out = initialiseDataLogger()
 
 # Initialise the plots object. Constructs and positions the required plots.
 plots = PlotFrames(plot_frame=True, plot_blur=False, plot_mask=True)
@@ -133,10 +85,10 @@ try:
 			# recentlyq, then switch to gateEdgeDetector
 			if cs_offset is not None:
 				# Solve the PnP problem using the 4 contour points
-				(cs_contour_sorted, rvec, tvec), euler = estimatePosePnP(cs_contour)
+				(cs_contour_sorted, rvec, tvec), (euler, tvec_camera) = estimatePosePnP(cs_contour)
 
 				# Update distance, plots and last gate find time
-				distance = tvec[2, 0]	# 3rd value in array (perpendicular)
+				distance = np.linalg.norm(tvec_camera)
 				mode = 1
 				plots.updatePlots(img, blur, mask_hsv, mode, distance=distance, 
 					gate_centre=(int(cs_x), int(cs_y)), rvec=rvec, tvec=tvec, 
@@ -159,9 +111,8 @@ try:
 			tello.new_frame = False
 			processed_frame = True		# This is just for the controller below
 
-
 			if log_data_user:
-				video_out.write(img)
+				config.video_out.write(img)
 		# Just display the raw frame if user is not tracking gate
 		elif not track_gate_user:
 			cv.imshow('Raw Frame', frame)
@@ -170,7 +121,13 @@ try:
 		if track_gate_user and controller_on_user:
 			# Update current waypoint and get vector to current waypoint
 			if optitrack:
-				true_state = trueState(streamingClient)
+				id_tello = trueState(streamingClient)[0][0]
+				pos_tello = trueState(streamingClient)[1][0,:]
+				rot_tello = trueState(streamingClient)[2][:,0].T
+				euler_tello = trueState(streamingClient)[3][0,:]
+				true_state = np.array([id_tello, pos_tello, rot_tello, euler_tello])
+				true_position_gate = trueState(streamingClient)[1][1,:]
+
 				relative_vector = waypointUpdate(streamingClient, true_state, waypoint)
 
 			end_time = time.time()
@@ -181,26 +138,54 @@ try:
 
 			# Run PID for gate tracking if gate was found (mode 1)
 			if mode == 1 and processed_frame:
-				estimated_state = estimatedState(rvec, tvec)
-				runPID(cs_width, cs_height, cs_offset, tvec, distance, dt, tello, 
+				estimated_state = estimatedState(rvec, tvec_camera)
+				runPID(cs_width, cs_height, cs_offset, tvec_camera, distance, dt, tello, 
 					euler, cs_contour_sorted)
 			# Run EdgeCont if edges were found within (mode 2)
 			elif mode == 2 and processed_frame:
 				runEdgeCont(ed_width, ed_offset, num_of_edges, distance, dt, tello)
 			# Run Waypoint tracking if optitrack is running
 			elif optitrack and processed_frame:
-				print('about to run waypoint')
 				runWaypoint(true_state, streamingClient, relative_vector, dt, tello)
 
-			processed_frame = False	# Done processing there for reset flag
 		start_time = time.time()
 
 		############################### MISC ###################################
 		# Log data if time elapsed is more than the log_rate
-		if start_time - time_datalog >= data_log_rate and log_data_user:
-			logData(log_file, start_time-time_initialise, true_state, 
-				estimated_state, mode, distance)
+		if start_time - time_datalog >= data_log_rate and log_data_user and processed_frame:
+			config.data_log['time (1)'] = start_time
+			if true_state is not None:
+				config.data_log['x_true (cm) (2)'] = true_state[1][0]
+				config.data_log['y_true (cm) (3)'] = true_state[1][1]
+				config.data_log['z_true (cm) (4)'] = true_state[1][2]
+				config.data_log['q0_true (5)'] = true_state[2][3]
+				config.data_log['q1_true (6)'] = true_state[2][0]
+				config.data_log['q2_true (7)'] = true_state[2][1]
+				config.data_log['q3_true (8)'] = true_state[2][2]
+				# Gate's position
+				config.data_log['x_true_gate (25)'] = true_position_gate[0]
+				config.data_log['y_true_gate (26)'] = true_position_gate[1]
+				config.data_log['z_true_gate (27)'] = true_position_gate[2]
+			else:
+				config.data_log['x_true (cm) (2)'] = None
+				config.data_log['y_true (cm) (3)'] = None
+				config.data_log['z_true (cm) (4)'] = None
+				config.data_log['q0_true (5)'] = None
+				config.data_log['q1_true (6)'] = None
+				config.data_log['q2_true (7)'] = None
+				config.data_log['q3_true (8)'] = None
+			
+			if mode == 1:
+				config.data_log['x_est (cm) (9)'] = tvec_camera[0]
+				config.data_log['y_est (cm) (10)'] = tvec_camera[1]
+				config.data_log['z_est (cm) (11)'] = tvec_camera[2]
+
+			config.data_log['mode (15)'] = mode
+			config.data_log['distance (cm) (16)'] = distance
+			logData(config.data_log)
 			time_datalog = time.time()
+
+		processed_frame = False	# Done processing therefore reset flag
 
 		# Display frame and check for user input. Note these user inputs only 
 		# work if a frame is displayed and selected.
@@ -210,8 +195,8 @@ try:
 			break
 
 		elif key == ord('s'):
-			cv.imwrite('Outputs/rawFrame'+str(screenshot_count)+'.png', frame)
-			# cv.imwrite('Outputs/processedFrame'+str(screenshot_count)+'.png', img)
+			# cv.imwrite('Outputs/rawFrame'+str(screenshot_count)+'.png', frame)
+			cv.imwrite('Outputs/processedFrame'+str(screenshot_count)+'.png', img)
 			# cv.imwrite('Outputs/maskFrame'+str(screenshot_count)+'.png', mask_hsv)
 			# cv.imwrite('Outputs/blurFrame'+str(screenshot_count)+'.png', blur)
 			screenshot_count += 1
@@ -239,8 +224,7 @@ try:
 			# https://stackoverflow.com/questions/12495218/using-user-input-to-call-functions
 			pass # manual tello command
 
-	log_file.close()		# close the logging file
-	video_out.release()		# release the video file
+	config.video_out.release()		# release the video file
 	tello.land()
 	tello.shutdown()
 	cv.destroyAllWindows()
@@ -254,12 +238,6 @@ except Exception:
 
 
 # implement user control into code,
-# Try lower gain on yaw from PnP
 
-# pt1 = [100, 100, 100]
-# pt2 = [150, 0, 100]
-# speed = 60
-# tello.curve(pt1, pt2, speed)
-
-# # If command causes error or "error Not Joystick", then reset rc and land
-# # or reset rc and continue with rest of script
+# If command causes error or "error Not Joystick", then reset rc and land
+# or reset rc and continue with rest of script
